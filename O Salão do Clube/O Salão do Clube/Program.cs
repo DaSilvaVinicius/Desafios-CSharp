e14093bce3bc153c003972c7384fec7ba97f5375
ViniOPrincesoGuerreiro <vinicius.da.silva@bol.com.br>
2022-04-26 10:27:33 -0300

    Recebendo os valores de entrada (O Salão do Clube)

--- O Salão do Clube/O Salão do Clube/Program.cs
+++ O Salão do Clube/O Salão do Clube/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿int comprimento = 0;
+int largura = 0;
+do
+{
+    string[] dimensoesDoSalao = Console.ReadLine().Split(' ');
+    comprimento = int.Parse(dimensoesDoSalao[0]);
+    largura = int.Parse(dimensoesDoSalao[1]);
+
+    if (comprimento != 0 && largura != 0)
+    {
+        int larguraDasTabuas = int.Parse(Console.ReadLine());
+        int numDeTabuas = int.Parse(Console.ReadLine());
+        string[] comprimentosDasTabuas = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < numDeTabuas; i++)
+        {
+            int comprimDaTabua = int.Parse(comprimentosDasTabuas[i]); 
+            
+        }
+    }
+} while (comprimento != 0 || largura != 0);
